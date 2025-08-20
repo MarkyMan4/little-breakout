0a3deab920923812e8830d9f7d5300b3ab13648b
MarkyMan4 <markus.hutnik@gmail.com>
2025-08-19 22:37:19 -0500

    level layout and paddle movement

--- src/main.js
+++ src/main.js
@@ -1,18 +1,35 @@
-import { engineInit, drawTextScreen, mainCanvasSize } from "littlejsengine";
+import { engineInit, EngineObject, vec2, randColor, cameraPos, canvasFixedSize, drawRect, Color, mousePos, clamp } from "littlejsengine";
 
-/*
-    Little JS Hello World Demo
-    - Just prints "Hello World!"
-    - A good starting point for new projects
-*/
+const levelSize = vec2(38, 20);
 
-'use strict';
+class Paddle extends EngineObject {
+    constructor() {
+        super(vec2(0, 1), vec2(6, 0.5));
+    }
+
+    update() {
+        this.pos.x = mousePos.x;
+        this.pos.x = clamp(this.pos.x, this.size.x / 2, levelSize.x - this.size.x / 2);
+    }
+}
 
 ///////////////////////////////////////////////////////////////////////////////
-function gameInit()
-{
-    // called once after the engine starts up
-    // setup the game
+function gameInit() {
+    canvasFixedSize.set(1280, 720);
+
+    const brickWidth = 2;
+    const brickHeight = 1;
+
+    for(let x = 2; x <= levelSize.x - 2; x += brickWidth) {
+        for(let y = 12; y <= levelSize.y - 2; y += brickHeight) {
+            const brick = new EngineObject(vec2(x, y), vec2(brickWidth, brickHeight));
+            brick.color = randColor();
+        }
+    }
+
+    cameraPos.set(levelSize.x / 2, levelSize.y / 2);
+
+    new Paddle();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -20,6 +37,8 @@ function gameUpdate()
 {
     // called every frame at 60 frames per second
     // handle input and update the game state
+    drawRect(cameraPos, vec2(100, 100), new Color(0.5, 0.5, 0.5));
+    drawRect(cameraPos, levelSize, new Color(0.1, 0.1, 0.1));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -41,7 +60,6 @@ function gameRenderPost()
 {
     // called after objects are rendered
     // draw effects or hud that appear above all objects
-    drawTextScreen('Hello World!', mainCanvasSize.scale(.5), 80);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
