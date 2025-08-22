25bafb2b8429b32ba667bb1aa0978dd6c8dc74d6
MarkyMan4 <markus.hutnik@gmail.com>
2025-08-21 22:01:23 -0500

    added ball, walls and collisions

--- src/main.js
+++ src/main.js
@@ -5,11 +5,31 @@ const levelSize = vec2(38, 20);
 class Paddle extends EngineObject {
     constructor() {
         super(vec2(0, 1), vec2(6, 0.5));
+        this.setCollision();
+        this.mass = 0;
     }
 
     update() {
         this.pos.x = mousePos.x;
         this.pos.x = clamp(this.pos.x, this.size.x / 2, levelSize.x - this.size.x / 2);
+    }
+}
+
+class Ball extends EngineObject {
+    constructor(pos) {
+        super(pos, vec2(0.5));
+        this.velocity = vec2(-0.1, -0.1);
+        this.setCollision();
+        this.elasticity = 1;
+    }
+}
+
+class Wall extends EngineObject {
+    constructor(pos, size) {
+        super(pos, size);
+        this.setCollision();
+        this.mass = 0;
+        this.color = new Color(0.5, 0.5, 0.5);
     }
 }
 
@@ -30,6 +50,10 @@ function gameInit() {
     setCameraPos(levelSize.scale(0.5));
 
     new Paddle();
+    new Ball(cameraPos);
+    new Wall(vec2(-0.5, levelSize.y / 2), vec2(1, 100)); // left
+    new Wall(vec2(levelSize.x / 2, levelSize.y + 0.75), vec2(100, 1)); // top
+    new Wall(vec2(levelSize.x + 0.5, levelSize.y), vec2(1, 100)); // right
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -37,8 +61,8 @@ function gameUpdate()
 {
     // called every frame at 60 frames per second
     // handle input and update the game state
-    drawRect(cameraPos, vec2(100, 100), new Color(0.5, 0.5, 0.5));
-    drawRect(cameraPos, levelSize, new Color(0.1, 0.1, 0.1));
+    // drawRect(cameraPos, vec2(100, 100), new Color(0.3, 0.3, 0.3));
+    drawRect(cameraPos, levelSize, new Color(0, 0, 0));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
